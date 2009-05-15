1492a95658d8f4846935f7c35f5b79fdde8b6fe9
Travis Parker <travis@slide.com>
2009-05-14 18:12:47 -0700

    enable re-scheduling the same greenlet with mainloop.schedule and friends

--- greenhouse/mainloop.py
+++ greenhouse/mainloop.py
@@ -46,23 +46,25 @@ def pause_until(unixtime):
 def pause_for(secs):
     pause_until(time.time() + secs)
 
-def schedule(func):
-    glet = greenlet(func)
+def schedule(run):
+    glet = isinstance(run, greenlet) and run or greenlet(run)
     glet.parent = generic_parent
     _state.paused.append(glet)
-    return func
+    return run
 
-def schedule_at(unixtime, func=None):
-    if func is None:
-        def decorator(func):
-            bisect.insort(_state.timed_paused, (unixtime, greenlet(func)))
-            return func
+def schedule_at(unixtime, run=None):
+    if run is None:
+        def decorator(run):
+            glet = isinstance(run, greenlet) and run or greenlet(run)
+            bisect.insort(_state.timed_paused, (unixtime, glet))
+            return run
         return decorator
-    bisect.insort(_state.timed_paused, (unixtime, greenlet(func)))
-    return func
+    glet = isinstance(run, greenlet) and run or greenlet(run)
+    bisect.insort(_state.timed_paused, (unixtime, glet))
+    return run
 
-def schedule_in(secs, func=None):
-    return schedule_at(time.time() + secs, func)
+def schedule_in(secs, run=None):
+    return schedule_at(time.time() + secs, run)
 
 @greenlet
 def generic_parent(ended):

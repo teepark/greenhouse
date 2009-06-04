dad254943acd65ad68c49396e54c1302d2f42903
Travis J Parker <travis.parker@gmail.com>
2009-06-03 18:45:33 -0700

    utility for the shell to display the state of paused greenlets

--- greenhouse/__init__.py
+++ greenhouse/__init__.py
@@ -4,3 +4,15 @@ from greenhouse.utils import Event, Lock, RLock, Condition, Semaphore,\
 from greenhouse.mainloop import pause, pause_until, pause_for, schedule,\
         schedule_at, schedule_in
 from greenhouse.sockets import Socket
+from greenhouse import _state
+
+
+def show_state():
+    print "%d greenlets awoken from events, waiting for their turn" \
+            % len(_state.awoken_from_events)
+    print "%d greenlets waiting on %d events" % (
+            sum(map(len, _state.paused_on_events.values())),
+            len(_state.paused_on_events.keys()))
+    print "%d greenlets waiting for timestamps" % len(_state.timed_paused)
+    print "%d greenlets cooperatively yielded, waiting for their turn" \
+            % len(_state.paused)

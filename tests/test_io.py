f8e09b0b5fd3d2d0309b1255c6ccc35884ecd7a7
Travis J Parker <travis.parker@gmail.com>
2009-09-19 06:54:19 -0700

    lots more greenhouse.Socket tests. now 75 total tests, 88% total coverage, 91% coverage of io.py

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,3 +1,4 @@
+import array
 import os
 import socket
 import stat
@@ -21,8 +22,6 @@ class MonkeyPatchingTestCase(StateClearingTestCase):
         assert socket.socket is greenhouse.Socket
 
     def test_unmonkeypatch(self):
-        greenhouse.unmonkeypatch()
-
         _sock = socket.socket
         _open = open
         _file = file
@@ -35,13 +34,17 @@ class MonkeyPatchingTestCase(StateClearingTestCase):
         assert file is _file
 
 class SocketTestCase(StateClearingTestCase):
-    def test_sockets_basic(self):
-        server = greenhouse.Socket()
-        client = greenhouse.Socket()
+    def makeserversock(self):
+        sock = greenhouse.Socket()
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        sock.bind(("", PORT))
+        sock.listen(5)
 
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind(("", PORT))
-        server.listen(5)
+        return sock
+
+    def test_sockets_basic(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
 
         client.connect(("", PORT))
         handler, addr = server.accept()
@@ -56,13 +59,190 @@ class SocketTestCase(StateClearingTestCase):
         client.close()
         server.close()
 
-    def test_sockets_btwn_grlets(self):
-        server = greenhouse.Socket()
+    def test_partial_recv(self):
+        server = self.makeserversock()
         client = greenhouse.Socket()
 
-        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        server.bind(("", PORT))
-        server.listen(5)
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        handler.send("this is a long message")
+
+        assert client.recv(4) == "this"
+        assert client.recv(3) == " is"
+        assert client.recv(2) == " a"
+        assert client.recv(5) == " long"
+        assert client.recv(40) == " message"
+
+    def test_recv_with_closed_sock(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.close()
+        self.assertRaises(socket.error, client.recv, 10)
+
+    def test_recvfrom(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.send("howdy")
+        assert handler.recvfrom(5)[0] == "howdy"
+
+        handler.send("hello, world")
+        assert client.recvfrom(12)[0] == "hello, world"
+
+        handler.close()
+        client.close()
+        server.close()
+
+    def test_recv_into(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        collector = array.array('c', '\0' * 5)
+        client.send("howdy")
+        handler.recv_into(collector, 5)
+        assert collector.tostring() == "howdy"
+
+        collector = array.array('c', '\0' * 12)
+        handler.send("hello, world")
+        client.recv_into(collector, 12)
+        assert collector.tostring() == "hello, world"
+
+        handler.close()
+        client.close()
+        server.close()
+
+    def test_recvfrom_into(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        collector = array.array('c', '\0' * 5)
+        client.send("howdy")
+        handler.recvfrom_into(collector, 5)
+        assert collector.tostring() == "howdy"
+
+        collector = array.array('c', '\0' * 12)
+        handler.send("hello, world")
+        client.recvfrom_into(collector, 12)
+        assert collector.tostring() == "hello, world"
+
+        handler.close()
+        client.close()
+        server.close()
+
+    def test_sendall(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.sendall("howdy")
+        assert handler.recv(5) == "howdy"
+
+        handler.sendall("hello, world")
+        assert client.recv(12) == "hello, world"
+
+        handler.close()
+        client.close()
+        server.close()
+
+    def test_sendto(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.sendto("howdy", ("", PORT))
+        assert handler.recv(5) == "howdy"
+
+        handler.close()
+        client.close()
+        server.close()
+
+    def test_sockopts(self):
+        sock = greenhouse.Socket()
+        assert sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR) == 0
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        assert sock.getsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR) == 1
+
+    # shutting down the reading end seems to have no effect on stdlib sockets,
+    # so verify that it has no effect on greenhouse.Sockets either
+    def test_shutdown_reading(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.shutdown(socket.SHUT_RD)
+
+        handler.send("hello again")
+        assert client.recv(11) == "hello again"
+
+    def test_shutdown_writing(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.shutdown(socket.SHUT_WR)
+
+        handler.send("hello")
+        assert client.recv(5) == "hello"
+
+        self.assertRaises(socket.error, client.send, "hello")
+
+    def test_shutdown_rdwr(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        client.shutdown(socket.SHUT_RDWR)
+
+        handler.send("hello")
+        assert client.recv(5) == ""
+
+        self.assertRaises(socket.error, client.send, "hello")
+
+    def test_fromsock(self):
+        server = self.makeserversock().dup()
+        client = greenhouse.Socket().dup()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+        handler = handler.dup()
+
+        client.send("howdy")
+        assert handler.recv(5) == "howdy"
+
+        handler.send("hello, world")
+        assert client.recv(12) == "hello, world"
+
+        handler.close()
+        client.close()
+        server.close()
+
+    def test_sockets_btwn_grlets(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
 
         grlet_results = []
 
@@ -78,6 +258,46 @@ class SocketTestCase(StateClearingTestCase):
         handler.send("hello to a greenlet")
         greenhouse.pause()
         assert grlet_results[0] == "hello to a greenlet"
+
+    def test_socketfile_read(self):
+        server = self.makeserversock()
+        client = greenhouse.Socket()
+
+        client.connect(("", PORT))
+        handler, addr = server.accept()
+
+        reader = handler.makefile()
+        results = []
+
+        @greenhouse.schedule
+        def f():
+            results.append(reader.read())
+
+        greenhouse.pause()
+        assert not results
+
+        client.send("this")
+        greenhouse.pause()
+        assert not results
+
+        client.send(" is")
+        greenhouse.pause()
+        assert not results
+
+        client.send(" a")
+        greenhouse.pause()
+        assert not results
+
+        client.send(" test")
+        greenhouse.pause()
+        assert not results
+
+        client.close()
+        greenhouse.pause()
+        assert results[0] == "this is a test"
+
+    def test_socket_timeout(self):
+        pass #TODO: working from here
 
 class FileTestCase(StateClearingTestCase):
     def setUp(self):

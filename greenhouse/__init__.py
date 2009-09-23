ac559d6951f9253b8b87f35a11f4b7d8be8cd929
Travis J Parker <travis.parker@gmail.com>
2009-09-22 18:22:43 -0700

    change up import order to mirror dependencies

--- greenhouse/__init__.py
+++ greenhouse/__init__.py
@@ -1,9 +1,9 @@
+from greenhouse._state import *
 from greenhouse.compat import *
-from greenhouse.utils import *
 from greenhouse.scheduler import *
+from greenhouse.utils import *
 from greenhouse.pool import *
 from greenhouse.io import *
-from greenhouse._state import *
 
 
 def show_state(): #pragma: no cover

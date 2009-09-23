f38a775184f8b3d56f5497a5d963443952cd1ddb
Travis J Parker <travis.parker@gmail.com>
2009-09-22 18:24:45 -0700

    don't depend on nose to run the tests

--- run_all_tests.py
+++ run_all_tests.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 
-import nose
-nose.main()
+import glob
+import os
+import sys
+import unittest
+
+import greenhouse
+
+
+suite = unittest.TestSuite()
+loader = unittest.TestLoader()
+runner = unittest.TextTestRunner()
+
+for fname in glob.glob(os.path.join("tests", "*.py")):
+    suite.addTest(loader.loadTestsFromName(fname.replace("/", ".")[:-3]))
+
+sys.exit(bool(runner.run(suite)))

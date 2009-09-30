acd2ddb3151148f81b65f8d757abb459dc83662e
Travis J Parker <travis.parker@gmail.com>
2009-09-30 14:59:37 -0700

    there is no more global state of greenlets blocked on events, so we can't show it

--- greenhouse/__init__.py
+++ greenhouse/__init__.py
@@ -9,9 +9,6 @@ from greenhouse.io import *
 def show_state(): #pragma: no cover
     print "%d greenlets awoken from events, waiting for their turn" \
             % len(state.awoken_from_events)
-    print "%d greenlets waiting on %d events" % (
-            sum(map(len, state.paused_on_events.values())),
-            len(state.paused_on_events.keys()))
     print "%d greenlets waiting for timestamps" % len(state.timed_paused)
     print "%d greenlets cooperatively yielded, waiting for their turn" \
             % len(state.paused)

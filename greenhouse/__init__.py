b7373e2272a20f675105da65af85b071ac6fa25b
Travis J Parker <travis.parker@gmail.com>
2010-01-15 15:14:06 -0800

    make that available globally

--- greenhouse/__init__.py
+++ greenhouse/__init__.py
@@ -4,4 +4,5 @@ from greenhouse.scheduler import *
 from greenhouse.utils import *
 from greenhouse.pool import *
 from greenhouse.io import *
+from greenhouse.scheduler import EXCEPTION_FILE as exception_file
 import greenhouse.poller

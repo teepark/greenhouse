2295452ffad89695112dfea6177f797dc1ee1605
Travis J Parker <travis.parker@gmail.com>
2010-01-16 10:50:22 -0800

    make sure the right ImportError gets raised so we aren't suggesting an outdated package

--- greenhouse/compat.py
+++ greenhouse/compat.py
@@ -1,8 +1,12 @@
 try:
     from greenlet import greenlet, GreenletExit
-except ImportError: #pragma: no cover
-    from py.magic import greenlet
-    GreenletExit = greenlet.GreenletExit
+except ImportError, error: #pragma: no cover
+    try:
+        from py.magic import greenlet
+        GreenletExit = greenlet.GreenletExit
+    except ImportError:
+        # suggest standalone greenlet, not the old py.magic.greenlet
+        raise error
 
 
 __all__ = ["greenlet", "main_greenlet", "GreenletExit"]

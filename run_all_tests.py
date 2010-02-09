f22d08bd90722049d32d1711d53f3f5c0994cd74
Travis J Parker <travis.parker@gmail.com>
2010-02-09 09:22:55 -0800

    we have a test failing only when it's run via nose. gonna have to figure that out later

--- run_all_tests.py
+++ run_all_tests.py
@@ -9,44 +9,41 @@ import unittest
 
 import greenhouse
 
-try:
-    from nose import main
-except ImportError:
-    def main():
-        parser = optparse.OptionParser()
-        parser.add_option("-v",
-                action="count",
-                dest="plusv",
-                help="Be more verbose.")
-        parser.add_option("-q",
-                action="count",
-                dest="minusv",
-                help="Be less verbose.")
-        parser.add_option("--verbosity",
-                default=1,
-                type=int,
-                help="Set verbosity; --verbose=2 is the same as -v")
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-v",
+            action="count",
+            dest="plusv",
+            help="Be more verbose.")
+    parser.add_option("-q",
+            action="count",
+            dest="minusv",
+            help="Be less verbose.")
+    parser.add_option("--verbosity",
+            default=1,
+            type=int,
+            help="Set verbosity; --verbose=2 is the same as -v")
 
-        options, args = parser.parse_args()
+    options, args = parser.parse_args()
 
-        options.verbosity += options.plusv or 0
-        options.verbosity -= options.minusv or 0
+    options.verbosity += options.plusv or 0
+    options.verbosity -= options.minusv or 0
 
-        suite = unittest.TestSuite()
-        loader = unittest.TestLoader()
-        runner = unittest.TextTestRunner(verbosity=options.verbosity)
+    suite = unittest.TestSuite()
+    loader = unittest.TestLoader()
+    runner = unittest.TextTestRunner(verbosity=options.verbosity)
 
-        for fname in glob.glob(os.path.join("tests", "*.py")):
-            suite.addTest(loader.loadTestsFromName(
-                fname.replace("/", ".")[:-3]))
+    for fname in glob.glob(os.path.join("tests", "*.py")):
+        suite.addTest(loader.loadTestsFromName(
+            fname.replace("/", ".")[:-3]))
 
-        dtfinder = doctest.DocTestFinder()
-        for fname in glob.glob(os.path.join("greenhouse", "*.py")):
-            mod = __import__(fname.replace("/", ".")[:-3])
-            if dtfinder.find(mod):
-                suite.addTest(doctest.DocTestSuite(mod))
+    dtfinder = doctest.DocTestFinder()
+    for fname in glob.glob(os.path.join("greenhouse", "*.py")):
+        mod = __import__(fname.replace("/", ".")[:-3])
+        if dtfinder.find(mod):
+            suite.addTest(doctest.DocTestSuite(mod))
 
-        sys.exit(runner.run(suite).wasSuccessful())
+    sys.exit(runner.run(suite).wasSuccessful())
 
 if __name__ == '__main__':
     main()

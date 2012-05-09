b437bfe553bc9f4448947851a52a9d7553e5bd4e
Travis J Parker <travis.parker@gmail.com>
2012-05-09 12:10:31 -0700

    version bump to 2.0.0

--- pavement.py
+++ pavement.py
@@ -3,7 +3,7 @@ from paver.path import path
 from paver.setuputils import setup
 
 
-VERSION = (1, 0, 0, "")
+VERSION = (2, 0, 0, "")
 
 setup(
     name="greenhouse",
@@ -19,7 +19,7 @@ setup(
     url="http://github.com/teepark/greenhouse",
     license="BSD",
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: BSD License",
         "Natural Language :: English",

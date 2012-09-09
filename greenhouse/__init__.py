a13b7b813f55215547ae061927f6c8452b513450
Travis J Parker <travis.parker@gmail.com>
2012-09-08 21:36:15 -0700

    version 2.1.0

--- greenhouse/__init__.py
+++ greenhouse/__init__.py
@@ -13,7 +13,7 @@ from greenhouse.backdoor import *
 from greenhouse.emulation import *
 
 
-VERSION = (2, 0, 1, '')
+VERSION = (2, 1, 0, '')
 
 __version__ = ".".join(filter(None, (str(x) for x in VERSION)))
 
--- pavement.py
+++ pavement.py
@@ -3,7 +3,7 @@ from paver.path import path
 from paver.setuputils import setup
 
 
-VERSION = (2, 0, 1, "")
+VERSION = (2, 1, 0, "")
 
 setup(
     name="greenhouse",
